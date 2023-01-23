9e220551c47542071044884fd1eee3742b390417
Graeme Gregory <graeme@xora.org.uk>
2023-01-23 19:28:59 +0000

    qdos.c : add console, name, stack definitions
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

---
+++ adv770-2.31/C/qdos.c
@@ -0,0 +1,17 @@
+#include <qdos.h>
+
+WINDOWDEF_t _condetails = {
+   2, /* border colour (red) */
+   0, /* border width */
+   0, /* paper (black) */
+   7, /* ink (white) */
+   512, /* width (pixel) */
+   256, /* height (pixels) */
+   0, /* x origin */
+   0 /* y origin */
+};
+
+char _prog_name[] = "Cave-2.31";
+
+long _stack = 20L * 1024L;
+
